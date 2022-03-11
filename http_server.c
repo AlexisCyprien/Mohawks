665f600bc15368faebe7fbff10db3bddc84660b5
Alexis Cyprien <derongreak@gmail.com>
2022-03-11 17:47:09 +0100

    Correction http_server.

--- http_server.c
+++ http_server.c
@@ -1,14 +1,14 @@
 #include "http_server.h"
 
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/limits.h>
 #include <poll.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <limits.h>
 #include <time.h>
-#include <fcntl.h>
-#include <errno.h>
 #include <unistd.h>
 
 #include "adresse_internet/adresse_internet.h"
@@ -29,6 +29,7 @@ int run_server(void) {
         return -1;
     }
     if (creerSocketEcouteTCP(secoute, "localhost", 80) != 0) {  // localhost ??
+        fprintf(stderr, "Erreur creerSocketEcouteTCP. \n");
         closeSocketTCP(secoute);
         return -1;
     }
@@ -61,7 +62,7 @@ int run_server(void) {
 
 void *treat_connection(void *arg) {
     if (arg == NULL) {
-        return;
+        return NULL;
     }
     SocketTCP *sservice = (SocketTCP *)arg;
     struct pollfd fds[1];
@@ -83,44 +84,44 @@ void *treat_connection(void *arg) {
 
         if (fds[0].revents & POLLIN) {
             size_t buflen = 4096;
-            char buffer = malloc(buflen);
-            if (buffer = NULL) {
+            char *buffer = malloc(buflen);
+            if (buffer == NULL) {
                 // Erreur à gerer
-                pthread_exit(EXIT_FAILURE);
+                pthread_exit(NULL);
             }
 
             if (readSocketTCP(sservice, buffer, buflen) == -1) {
                 // Err
-                pthread_exit(EXIT_FAILURE);
+                pthread_exit(NULL);
             }
             // Lecture requete
 
             http_request *request = malloc(sizeof *request);
             if (request == NULL) {
                 // Err
-                pthread_exit(EXIT_FAILURE);
+                pthread_exit(NULL);
             }
             if (init_request(request) != 0) {
                 // Err
-                pthread_exit(EXIT_FAILURE);
+                pthread_exit(NULL);
             }
 
             int r = parse_http_request(buffer, request);
             if (r != 0) {
                 // Err
-                pthread_exit(EXIT_FAILURE);
+                pthread_exit(NULL);
             }
 
             r = treat_http_request(sservice, request);
             if (r != 0) {
                 // Err
-                pthread_exit(EXIT_FAILURE);
+                pthread_exit(NULL);
             }
 
             // free etc
         }
     }
-
+    return NULL;
     // Bye bye
 }
 
@@ -128,66 +129,65 @@ int treat_http_request(SocketTCP *sservice, http_request *request) {
     if (sservice == NULL || request == NULL) {
         return ERR_NULL;
     }
-    
 
     if (strcmp(request->request_line->method, "GET") == 0) {
-
         char *ret = strstr(request->request_line->uri, "../");
-        if (ret != NULL) { 
-            if (send(sservice->sockfd, FORBIDEN_RESP, sizeof(FORBIDEN_RESP), 0) < sizeof(FORBIDEN_RESP)) {
+        if (ret != NULL) {
+            if (send(sservice->sockfd, FORBIDEN_RESP, sizeof(FORBIDEN_RESP),
+                     0) < (ssize_t)sizeof(FORBIDEN_RESP)) {
                 // Err
             }
         } else {
             char path[PATH_MAX] = ".";
-            strncat(&path, request->request_line->uri, sizeof(path));
-            strncat(&path, INDEX, sizeof(path));
+            strncat(path, request->request_line->uri, sizeof(path) - 1);
+            strncat(path, INDEX, sizeof(path) - 1);
 
             time_t t;
-            if (time(&t) == (time_t) -1) {
+            if (time(&t) == (time_t)-1) {
                 perror("time");
             }
             struct tm readable_time;
             localtime_r(&t, &readable_time);
             char time[200] = "Date: ";
-            strftime(&time[strlen(time)], sizeof(time), "%a, %d %b %Y %T %Z", &readable_time);
-            strncat(&time, CRLF, sizeof(time));
+            strftime(&time[strlen(time)], sizeof(time), "%a, %d %b %Y %T %Z",
+                     &readable_time);
+            strncat(time, CRLF, sizeof(time) - 1);
 
             char server[200] = "Server: ";
-            strncat(&server, SERVER_NAME, sizeof(server));
+            strncat(server, SERVER_NAME, sizeof(server) - 1);
 
             char resp[4096];
-            strncpy(&resp, OK_RESP, sizeof(resp));
-            strncat(&resp, &time, sizeof(resp));
-            strncat(&resp, &server, sizeof(resp));
+            strncpy(resp, OK_RESP, sizeof(resp));
+            strncat(resp, time, sizeof(resp) - 1);
+            strncat(resp, server, sizeof(resp) - 1);
 
             int index_fd;
             errno = 0;
-            if ((index_fd = open(&path, O_RDONLY)) == -1) {
-                if (errno == ENOENT ) {
-                    if (send(sservice->sockfd, NOT_FOUND_RESP, sizeof(NOT_FOUND_RESP), 0) < sizeof(NOT_FOUND_RESP)) {
+            if ((index_fd = open(path, O_RDONLY)) == -1) {
+                if (errno == ENOENT) {
+                    if (send(sservice->sockfd, NOT_FOUND_RESP,
+                             sizeof(NOT_FOUND_RESP),
+                             0) < (ssize_t)sizeof(NOT_FOUND_RESP)) {
                         // Err
                     }
                 } else {
-                    //Err
+                    // Err
                 }
             }
-            char body[4096];
+            char body[2048];
             if (read(index_fd, &body, sizeof(body)) == -1) {
-                //Err
+                // Err
             }
-            strncat(&resp, &body, sizeof(resp));
-            strncat(&resp, CRLF, sizeof(resp));
-            strncat(&resp, CRLF, sizeof(resp));
+            strncat(resp, body, sizeof(resp) - 1);
+            strncat(resp, CRLF, sizeof(resp) - 1);
+            strncat(resp, CRLF, sizeof(resp) - 1);
 
             if (send(sservice->sockfd, &resp, sizeof(resp), 0) == 1) {
-                //Err
+                // Err
             }
 
             pthread_exit(NULL);
         }
-        
-        
-
     }
 
     return 0;
